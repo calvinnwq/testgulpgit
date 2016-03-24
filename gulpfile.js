57ec826728e8180e53c7303821a9e0430e9b203e
Calvin Ng <cng@atlassian.com>
2016-03-24 12:57:55 +1100

    test commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
 var gulp = require('gulp');
 var argv = require('yargs').argv;
 var git = require('gulp-git');
+
+gulp.task('init', function() {
+  console.log(argv.m);
+});
+
+gulp.task('add', function() {
+  console.log('adding...');
+  return gulp.src('.')
+    .pipe(git.add());
+});
+
+gulp.task('commit', function() {
+  console.log('commiting');
+  if (argv.m) {
+    return gulp.src('.')
+      .pipe(git.commit(argv.m));
+  }
+});
+
+gulp.task('gitsend', ['add', 'commit'], function() {
+  console.log('adding, commiting and pushing to git...');
+});
